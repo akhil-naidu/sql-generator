1d64e2135115c28e738620bbb1981b27c6006d7b
akhil-naidu <kaparapu.akhilnaidu@gmail.com>
2022-10-12 14:18:01 +0530

    chore: updating test.js

--- test.js
+++ test.js
@@ -1,6 +1,11 @@
 import SQLObject from './index.js';
 
 const usersObject = new SQLObject('users');
-const userSQL = usersObject.createColumn('id', 'INT').generateSQL();
+const userSQL = usersObject
+  .createColumn('id', 'INT')
+  .addArgs(['NOT NULL', 'AUTO_INCREMENT'])
+  .createColumn('name', 'TEXT')
+  .addArg('NOT NUll')
+  .generateSQL();
 
 console.log(userSQL);
